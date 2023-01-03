a12aa5e3e4d3ce66c7945ad45da3b68d215c05bd
Iuri Johansson <iuri_abreu@hotmail.com>
2023-01-03 18:50:18 +0000

    Add a challenge example to myscript.js

--- myscript.js
+++ myscript.js
@@ -1,6 +1,106 @@
 $(document).ready(function() {
-    // $("h2").css("text-decoration", "underline"); //underlines all <h2> elements
-    // $("ul").css("border", "solid 1px #ccc"); //adds a border to all lists
-    $("h2").addClass("underline"); //underlines all <h2> elements
-    $("ul").addClass("border"); //adds a border to all lists
+    // $("h2").css("text-decoration", "underline"); // underlines all <h2> elements
+    // $("ul").css("border", "solid 1px #ccc"); // adds a border to all lists
+    // $("h2").addClass("underline"); // adds underline class
+    // $("ul").addClass("border"); // adds border class
+    // $("h2").removeClass("underline"); // removes underline class
+    // $("ul").removeClass("border"); // removes border class
+
+    //example Writing Our First JQuery Script Challenge 1
+//     <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <title>Changing HTML/CSS with JQuery</title>
+//   <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+//   <link href="index.css" rel="stylesheet" type="text/css">
+  
+// </head>
+// <body>
+//   <!--create a table-->
+//   <table>
+//     <tr>
+//       <th></th>
+//       <th>Country</th>
+//       <th>Price</th>
+//       <th>Nutrition</th>
+//       <th>Category</th>
+//     </tr>
+//     <tr>
+//       <th class="highlight">Spinach</th>
+//       <td>America</td>
+//       <td>28</td>
+//       <td>90</td>
+//       <td>Veg</td>
+//     </tr>
+//     <tr>
+//       <th class="highlight">Carrots</th>
+//       <td>France</td>
+//       <td>56</td>
+//       <td>75</td>
+//       <td>Veg</td>
+//     </tr>
+//     <tr>
+//       <th class="highlight">Broccoli</th>
+//       <td>Bulgaria</td>
+//       <td>12</td>
+//       <td>70</td>
+//       <td>Veg</td>
+//     </tr>
+//     <tr>
+//       <th class="highlight">Orange</th>
+//       <td>Morocco</td>
+//       <td>15</td>
+//       <td>50</td>
+//       <td>Fruit</td>
+//     </tr>
+//     <tr>
+//       <th class="highlight">Tangerine</th>
+//       <td>Cyprus</td>
+//       <td>12</td>
+//       <td>25</td>
+//       <td>Fruit</td>
+//     </tr>
+//   </table>
+  
+//   <script
+//   src="https://code.jquery.com/jquery-3.2.1.js"
+//   integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+//   crossorigin="anonymous"></script>
+//   <script src="index.js"></script>
+// </body>
+// </html>
+
+/* css
+td, th {
+  width: 5rem;
+  height: 2rem;
+  border: 1px solid #ccc;
+  text-align: center;
+}
+th {
+  background: #48bcf3;
+  border-color: white;
+}
+
+th.highlight {
+  background: lightblue;
+  border-color: white;
+}
+body {
+  padding: 1rem;
+  font-family: Roboto;
+}
+.odd {
+  background-color: #E84610;
+}
+
+.even {
+  background-color: #C65353;
+} */
+
+//index.js
+// $("tr:odd").addClass("odd");
+// $("tr:even").addClass("even");
+
 });
