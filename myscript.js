89dcf0a5d2273de5b9f9046c886724236711fda4
Iuri Johansson <iuri_abreu@hotmail.com>
2023-01-03 18:39:26 +0000

    Add examples to myscript.js

---
+++ myscript.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // $("h2").css("text-decoration", "underline"); //underlines all <h2> elements
+    // $("ul").css("border", "solid 1px #ccc"); //adds a border to all lists
+    $("h2").addClass("underline"); //underlines all <h2> elements
+    $("ul").addClass("border"); //adds a border to all lists
+});
